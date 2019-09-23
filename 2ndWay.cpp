c857e34fc6d895b6156f391c49819a1e129aa36c
uNKoLL Bro <marian_eu_cata@outlook.com>
2019-09-23 22:11:00 +0200

    Update 2ndWay.cpp

--- 2ndWay.cpp
+++ 2ndWay.cpp
@@ -2,16 +2,25 @@
 
 using namespace std;
 
-int contarCaracteres(unsigned int num){
-    int digitos = 0;
+//FUNCION QUE RECIBE EL PARÁMETRO numRecibido
+int contarCaracteres(unsigned int numRecibido){
+    //CONTADOR DE DIGITOS
+    int contador = 0;
+    //BUCLE DO WHILE : MIENTRAS EL numRecibido EXISTA (MAYOR QUE 0) ESTARA EN FUNCIONAMIENTO..
     do {
-         digitos++;
-         num /= 10;
-    } while (num);
-    return digitos;
+        //CADA VEZ QUE SE ENTRA EN EL BUCLE, SUBEN LOS DIGITOS POR LO TANTO, CONTADOR SUBE
+         contador++;
+        //PARA EVITAR BUCLE INFINO, EL NUMERO RECIBIDO SE DIVIDE ENTRE SU BASE (10) DE ESTA FORMA BAJAMOS DIGITOS
+         numRecibido /= 10;
+        //EL BUCLE FINALIZA CUANDO EL NUMERO ES COMPLETADO ( 0 )
+    } while (numRecibido);
+
+    //LA FUNCION contarCaracteres DEVUELVE CONTADOR
+    return contador;
 }
 
 int main(){
-    cout << contarCaracteres(7);
+    //MOSTRAMOS POR PANTALLA LA FUNCION contarCaracteres que recibe como parámetros UNSIGNED INT , en este caso 2001
+    cout << contarCaracteres(2001);
     return 0;
 }
